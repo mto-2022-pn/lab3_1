6223cbb68164ad316dee597d4f3637541dc36338
Jakub Wlaźlak <kubikapb@gmail.com>
2022-03-28 18:28:12 +0200

    singlePositionRequestReturnSinglePositionInvoice test added

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,20 +1,49 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class BookKeeperTest {
+    @Mock
+    private InvoiceFactory invoiceFactory;
+    @Mock
+    private TaxPolicy taxPolicy;
+
+    private InvoiceRequest invoiceRequest;
+    private BookKeeper bookKeeper;
+    private ClientData clientData;
 
     @BeforeEach
-    void setUp() throws Exception {
-
+    void setUp() {
+        bookKeeper = new BookKeeper(invoiceFactory);
+        clientData = new ClientData(Id.generate(), "client");
+        invoiceRequest = new InvoiceRequest(clientData);
     }
 
     @Test
-    void test() {
-        fail("not implemented");
-    }
+    public void singlePositionRequestReturnSinglePositionInvoice() {
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(new Tax(Money.ZERO, "tax"));
+        when(invoiceFactory.create(clientData)).thenReturn(new Invoice(Id.generate(), clientData));
 
+        ProductBuilder productBuilder = new  ProductBuilder();
+        RequestItemBuilder requestItemBuilder = new RequestItemBuilder();
+        Product product = productBuilder.build();
+        RequestItem requestItem = requestItemBuilder.withProductData(product.generateSnapshot()).build();
+        invoiceRequest.add(requestItem);
+        int result = bookKeeper.issuance(invoiceRequest,taxPolicy).getItems().size();
+        assertEquals(1,result);
+    }
 }
