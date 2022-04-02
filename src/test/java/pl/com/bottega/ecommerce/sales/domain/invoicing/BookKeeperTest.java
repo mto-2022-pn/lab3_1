9aa4e61395482febdb9529ee587c518d22099813
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-04-02 17:44:56 +0200

    Added second test case from exercise.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,8 +1,9 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,8 @@ import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.util.function.BooleanSupplier;
 
 @ExtendWith(MockitoExtension.class)
 class BookKeeperTest {
@@ -30,9 +33,20 @@ class BookKeeperTest {
     void testCase1() {
         when(taxPolicy.calculateTax(
                 any(ProductType.class), any(Money.class))).thenReturn(new Tax(Money.ZERO, "unknown"));
-        InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(new Id("222440"), "Szymon"));
-        invoiceRequest.add(new RequestItemBuilder().build());
-        Invoice invoice = this.bookKeeper.issuance(invoiceRequest, taxPolicy);
+        InvoiceRequest request = new InvoiceRequest(new ClientData(new Id("222440"), "Szymon"));
+        request.add(new RequestItemBuilder().build());
+        Invoice invoice = this.bookKeeper.issuance(request, taxPolicy);
         assertEquals(invoice.getItems().size(), 1);
+    }
+
+    @Test
+    void testCase2() {
+        when(taxPolicy.calculateTax(
+                any(ProductType.class), any(Money.class))).thenReturn(new Tax(Money.ZERO, "unknown"));
+        InvoiceRequest request = new InvoiceRequest(new ClientData(new Id("222440"), "Szymon"));
+        request.add(new RequestItemBuilder().build());
+        request.add(new RequestItemBuilder().build());
+        this.bookKeeper.issuance(request, taxPolicy);
+        verify(taxPolicy, times(2)).calculateTax(any(ProductType.class), any(Money.class));
     }
 }
