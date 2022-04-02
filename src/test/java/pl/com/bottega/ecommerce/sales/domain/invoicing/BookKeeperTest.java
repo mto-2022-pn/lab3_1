612be1101654239e989db82d6475e41476c1b964
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-04-02 20:07:37 +0200

    Added two tests for empty request case.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -9,7 +9,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
@@ -18,6 +22,7 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 import java.util.function.BooleanSupplier;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class BookKeeperTest {
 
     @Mock
@@ -48,5 +53,21 @@ class BookKeeperTest {
         request.add(new RequestItemBuilder().build());
         this.bookKeeper.issuance(request, taxPolicy);
         verify(taxPolicy, times(2)).calculateTax(any(ProductType.class), any(Money.class));
+    }
+
+    @Test
+    void testReturnWithEmptyRequest() {
+        InvoiceRequest request = new InvoiceRequest(new ClientData(new Id("222440"), "Szymon"));
+        Invoice invoice = this.bookKeeper.issuance(request, null);
+        assertEquals(invoice.getItems().size(), 0);
+    }
+
+    @Test
+    void testBehaviorWithEmptyRequest() {
+        when(taxPolicy.calculateTax(
+                any(ProductType.class), any(Money.class))).thenReturn(new Tax(Money.ZERO, "unknown"));
+        InvoiceRequest request = new InvoiceRequest(new ClientData(new Id("222440"), "Szymon"));
+        this.bookKeeper.issuance(request, taxPolicy);
+        verify(taxPolicy, never()).calculateTax(any(ProductType.class), any(Money.class));
     }
 }
