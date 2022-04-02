3707e94a3a53f31d1c5bfa9c987531ef86e694ba
MateuszMatusiak <mateusz.matusiak000@gmail.com>
2022-04-03 01:52:19 +0200

    ProductBuilder

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/Product.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/productscatalog/Product.java
@@ -14,11 +14,11 @@ public class Product extends BaseAggregateRoot {
 
     private ProductType productType;
 
-    public Product(Id aggregateId, Money price, String name, ProductType productType) {
-        this.id = aggregateId;
-        this.price = price;
-        this.name = name;
-        this.productType = productType;
+    private Product(final ProductBuilder builder) {
+        this.id = builder.id;
+        this.price = builder.price;
+        this.name = builder.name;
+        this.productType = builder.productType;
     }
 
     public boolean isAvailable() {
@@ -40,4 +40,38 @@ public class Product extends BaseAggregateRoot {
     public ProductData generateSnapshot() {
         return new ProductData(getId(), price, name, productType, new Date());
     }
+
+    static class ProductBuilder {
+        private Id id;
+        private Money price;
+        private String name;
+        private ProductType productType;
+
+        public ProductBuilder id(Id id) {
+            this.id = id;
+            return this;
+        }
+
+        public ProductBuilder price(Money price) {
+            this.price = price;
+            return this;
+        }
+
+        public ProductBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public ProductBuilder productType(ProductType productType) {
+            this.productType = productType;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
+
+    }
 }
+
+
