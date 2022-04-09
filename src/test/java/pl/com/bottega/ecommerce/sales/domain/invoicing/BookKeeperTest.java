05bab583ddd88c267f389f32389dc410f0d2a61a
JakZaj <jakubzajac13@gmail.com>
2022-04-09 14:35:54 +0200

    Added four tests and cleared repeated code

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
@@ -17,6 +19,7 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class BookKeeperTest {
 
     @Mock
@@ -24,21 +27,18 @@ class BookKeeperTest {
 
     private BookKeeper bookKeeper;
     private InvoiceRequest invoiceRequest;
-    private ProductDataBuilder productDataBuilder;
 
     @BeforeEach
-    void setUp() throws Exception {
+    void setUp() {
         bookKeeper = new BookKeeper(new InvoiceFactory());
         ClientData clientData = new ClientData(Id.generate(), "Client");
         invoiceRequest = new InvoiceRequest(clientData);
-        productDataBuilder = new ProductDataBuilder();
+        Mockito.when(taxPolicy.calculateTax(Mockito.any(ProductType.class), Mockito.any(Money.class)))
+                .thenReturn(new Tax(Money.ZERO, "Tax"));
     }
 
     @Test
     void testCase1() {
-        Mockito.when(taxPolicy.calculateTax(Mockito.any(ProductType.class), Mockito.any(Money.class)))
-                .thenReturn(new Tax(Money.ZERO, "Tax"));
-
         invoiceRequest.add(new RequestItemBuilder().build());
         Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
 
@@ -47,15 +47,52 @@ class BookKeeperTest {
 
     @Test
     void testCase2() {
-        Mockito.when(taxPolicy.calculateTax(Mockito.any(ProductType.class), Mockito.any(Money.class)))
-                .thenReturn(new Tax(Money.ZERO, "Tax"));
-
         for(int i = 0; i < 2; i++) {
             invoiceRequest.add(new RequestItemBuilder().build());
         }
         bookKeeper.issuance(invoiceRequest, taxPolicy);
 
         Mockito.verify(taxPolicy, Mockito.times(2))
+                .calculateTax(Mockito.any(ProductType.class),Mockito.any(Money.class));
+    }
+
+    @Test
+    void testZeroInvoiceRequest() {
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        assertEquals(0, invoice.getItems().size());
+    }
+
+    @Test
+    void testZeroInvoke() {
+
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        Mockito.verify(taxPolicy, Mockito.never())
+                .calculateTax(Mockito.any(ProductType.class),Mockito.any(Money.class));
+    }
+
+    @Test
+    void testDiffrentProductInvoicesRequest() {
+        ProductDataBuilder productDataBuilder = new ProductDataBuilder();
+        invoiceRequest.add(new RequestItemBuilder().withProductDataBuilder(productDataBuilder.withProductType(ProductType.DRUG)).withQuantity(15).build());
+        invoiceRequest.add(new RequestItemBuilder().withProductDataBuilder(productDataBuilder.withProductType(ProductType.FOOD).withName("Apple")).withQuantity(8).build());
+        invoiceRequest.add(new RequestItemBuilder().withProductDataBuilder(productDataBuilder.withProductType(ProductType.STANDARD)).withQuantity(24).build());
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        assertEquals(3, invoice.getItems().size());
+    }
+
+    @Test
+    void testDiffrentProductInvokes() {
+        ProductDataBuilder productDataBuilder = new ProductDataBuilder();
+        invoiceRequest.add(new RequestItemBuilder().withProductDataBuilder(productDataBuilder.withProductType(ProductType.DRUG)).withQuantity(12).build());
+        invoiceRequest.add(new RequestItemBuilder().withProductDataBuilder(productDataBuilder.withProductType(ProductType.FOOD).withName("Banana")).withQuantity(10).build());
+        invoiceRequest.add(new RequestItemBuilder().withProductDataBuilder(productDataBuilder.withProductType(ProductType.STANDARD)).withQuantity(30).build());
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        Mockito.verify(taxPolicy, Mockito.times(3))
                 .calculateTax(Mockito.any(ProductType.class),Mockito.any(Money.class));
     }
 }
