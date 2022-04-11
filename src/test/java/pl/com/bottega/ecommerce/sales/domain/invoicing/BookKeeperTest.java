3eec5eab1871a371497fb331513f5a420ac2e229
Konrad Basta <konrad.basta@onet.com.pl>
2022-04-11 16:24:41 +0200

    Added test case 1

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,20 +1,49 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
+@ExtendWith(MockitoExtension.class)
 class BookKeeperTest {
+
+    private final String DEFAULT = "default";
+    private final String ID = "12345678";
+    private final String NAME = "JOHN";
+
+    @Mock
+    private TaxPolicy taxPolicy;
+    private BookKeeper bookKeeper;
+    private InvoiceRequest invoiceRequest;
 
     @BeforeEach
     void setUp() throws Exception {
+        bookKeeper = new BookKeeper(new InvoiceFactory());
 
+        invoiceRequest = new InvoiceRequest(
+                new ClientData(new Id(ID), NAME)
+        );
+
+        when(taxPolicy.calculateTax(
+                any(ProductType.class), any(Money.class)
+        )).thenReturn(new Tax(Money.ZERO, DEFAULT));
     }
 
     @Test
-    void test() {
-        fail("not implemented");
+    void requestInvoiceWithOneItem() {
+        invoiceRequest.add(new RequestItemBuilder().build());
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        assertEquals(invoice.getItems().size(), 1);
     }
-
 }
