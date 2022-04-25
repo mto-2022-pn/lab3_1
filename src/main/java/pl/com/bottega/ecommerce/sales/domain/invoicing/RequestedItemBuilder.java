edec278389663d6bd758fd1854a1dbe863cd10f9
Karol Profic <karolprofic@protonmail.com>
2022-04-25 09:04:42 +0200

    Dodanie RequestedItemBuilder

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/RequestedItemBuilder.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/RequestedItemBuilder.java
@@ -1,4 +1,30 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductBuilder;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class RequestedItemBuilder {
+    private ProductData productData = new ProductBuilder().build().generateSnapshot();
+    private Money totalCost = Money.ZERO;
+    private int quantity = 1;
+
+    public RequestItem build(){
+        return new RequestItem(this.productData, this.quantity, this.totalCost);
+    }
+
+    public RequestedItemBuilder withProductData(ProductData productData){
+        this.productData = productData;
+        return this;
+    }
+
+    public RequestedItemBuilder withQuantity(int quantity){
+        this.quantity = quantity;
+        return this;
+    }
+
+    public RequestedItemBuilder withTotalCost(Money totalCost){
+        this.totalCost = totalCost;
+        return this;
+    }
+
 }
